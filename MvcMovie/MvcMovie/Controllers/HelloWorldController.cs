6a23561a2f82508c101589db31d78ca67c961b5a
tiagofsantos <tiago.afsantos@hotmail.com>
2018-10-14 15:09:36 +0100

    Alterei o método Welcome
    Welcome recebe agora um ID, em vez de  numTimes

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -16,9 +16,9 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
